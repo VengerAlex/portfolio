e52aedde36075a8f817638f51e501b1bc63aee5d
Ivan Mukoed <1rydler@gmail.com>
2021-12-23 09:50:55 +0200

    First version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Nav from "./components/Nav";
+
+import {
+  HashRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App bg-gray-800 text-white min-h-screen">
+        <Nav />
+        <Routes >
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/about" element={<About />} />
+          <Route exact path="/" element={<Hero />} />
+        </Routes >
+        <Footer />
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+const About = () => {
+    return (
+
+        <div className="md:px-20 px-1">
+            <div className="ml-8 text-gray-300 text-xl">About Me</div>
+            <div className="border-left-gradient ml-3 pl-5 my-5 text-center">My name is Ivan Mukoed I'm a Front-End Engineer based in Kiev☀️. I describe myself as a passionate developer who loves coding, open source, his pet projects and the web itself. I have 3 years of experience working in small companies and freelancing.
+                While working on different tasks for different clients, I acquired a lot of useful skills, as well as the ability to help your websites reach their maximum potential. Whatever you’ve got for me - I’m ready. Contact me now to up your web game a whole new level.</div>
+            <div className="border-right-gradient ml-3 pr-5 my-5 text-center">I work to make a better web; one that is fast, easy to use, beautiful, accessible to all, and frustration-free. Regardless of your specific business requirements, in solving these challenges, you have a great chance of finding success online.
+                My main experience is in front-end development but I also have a passion for design. Given that producing a modern website requires the combination of design, server technology, and the layer that users interacts with, I believe having experience in both design and development allows for making the most optimal user experiences.</div>
+            <div className="border-left-gradient ml-3 pl-5 my-5 text-center">When working I prefer to use modern frontend technologies and frameworks, CSS-based (Tailwind, MaterialUI, Bootstrap), JS-based (React, Vue, Angular, Next, Nuxt etc) and, of course, the one that help me with design (Figma, Photoshop, Unsplash).
+                They help me achieve the best result in the shortest amount of time possible.</div>
+        </div>
+
+    )
+}
+
+export default About
